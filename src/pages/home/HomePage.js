a0f8df7bc01429c9196590a228a329cd30aac66c
Taeyoung20230727 <qksrm5546@gmail.com>
2024-07-03 17:43:45 +0900

    연결 확인

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,19 @@
+import axios from 'axios';
 import React from 'react'
 
 const HomePage = () => {
+
+  axios.get(`http://localhost:8080/pokeguide/users`)
+  .then((data) => {
+    console.log(data)
+  }).catch((err) => {
+    console.log(err);
+  });
+
   return (
     <div>
       <h2>로그인페이지</h2>
+
       <img src="../images/pikachu.gif" alt='피카츄'></img>
     </div>
   )
