2b59d0c51577c6f6603962a1d27666f034227863
Taeyoung20230727 <qksrm5546@gmail.com>
2024-07-17 14:17:33 +0900

    register

--- src/router/root.js
+++ src/router/root.js
@@ -4,7 +4,10 @@ import MainPage from "../pages/main/MainPage";
 import LoginPage from "../pages/user/LoginPage";
 import ListPage from "../pages/pokemons/ListPage";
 import DashboardPage from "../pages/admin/DashboardPage";
-
+import Terms from "../pages/user/Terms";
+import Register from "../pages/user/Register";
+import FindId from "../pages/user/FindId";
+import FindPass from "../pages/user/FindPass";
 
 //router 생성
 const root = createBrowserRouter([
@@ -13,14 +16,16 @@ const root = createBrowserRouter([
     
     // user
     {path: "/user/login", element:<LoginPage />}, // 로그인
-
+    {path: "/user/Terms", element:<Terms />}, // 약관
+    {path: "/user/register", element:<Register />}, // 회원가입
+    {path: "/user/FindId", element:<FindId />}, // 아이디찾기
+    {path: "/user/FindPass", element:<FindPass />}, // 비번찾기
 
     // pokemons
     {path: "/pokemons/list", element:<ListPage />}, // 포켓몬 목록
 
     // admin
     {path: "/admin/dashboard", element:<DashboardPage />}, // 관리자 대시보드
-
 ]);
 
 export default root;
