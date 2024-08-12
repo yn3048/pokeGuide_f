24553dd9a0190b91f5c6e74f906b0666492a7838
Taeyoung20230727 <qksrm5546@gmail.com>
2024-08-12 17:55:50 +0900

    채팅 이미지업로드

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -26,7 +26,6 @@
         
                     navigate('/');
                 } catch (error) {
-                    console.error('Login failed', error);
                     setError('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
                 }
             }
