31349efadaf0c11721a87c8172605e844631398f
dpsk3048 <dpsk3048@naver.com>
2024-09-03 01:30:21 +0900

    헤더

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -10,23 +10,37 @@ const Header = () => {
           <img src='/images/main_logo.png' alt='logo'/>
           </Link>
         </h1>
-        <div>
-        <div>
-          <Link to="/poke/list">
-          <img src='/images/pokeball.png' alt='pokeball'/>
-          </Link>
-          <Link to="/">
-          <img src='/images/board.png' alt='board'/>
-          </Link>
-        <Link to="/user/login">
-          <img src='/images/login.png' alt='login'/>
-          </Link>
-          <Link to="/">
-          <img src='/images/search.png' alt='search'/>
-          </Link>
-        </div>
-        </div>
        
+        <div className='nav-box'>
+          <ul className='gnb'>
+            <li>
+            <Link to="/poke/list">
+            <img src='/images/pokeball.png' alt='pokeball'/>
+            <p>포켓몬 도감</p>
+            </Link>
+            </li>
+            <li>
+            <Link to="/">
+            <img src='/images/board.png' alt='board'/>
+            <p>게시판</p>
+            </Link>
+            </li>   
+          </ul>
+         <ul className='right-gnb'>
+          <li>
+            <Link to="/user/login">
+            <img src='/images/login.png' alt='login'/>
+            <p>로그인</p>
+          </Link>
+          </li>
+          <li>
+            <Link to="/">
+            <img src='/images/search.png' alt='search'/>
+            <p>통합검색</p>
+            </Link>
+          </li>
+         </ul>
+        </div>
       </header>
       <hr/>
     </>
